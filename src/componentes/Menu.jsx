5d607078564a76f0eb7f610f5f0ef8871ba1bdb5
migueljgc <miguelgaviriam.8@gmail.com>
2024-10-04 16:06:21 -0500

    act menus con logo provicional

--- src/componentes/Menu.jsx
+++ src/componentes/Menu.jsx
@@ -42,44 +42,44 @@ export const Menu = () => {
     };
   }, []);
   return (
-      <div>
-        <nav className="nav">
+    <div>
+      <nav className="nav">
 
-          <ul className="list">
-            <Logo/>
+        <ul className="list">
+          <Logo />
 
-            <li className="list__item">
-              <div className="list__button">
-                <img src="assets/dashboard.svg" className="list__img" />
-                <a href="/HomePage" className="nav__link">Inicio</a>
-              </div>
-            </li>
+          <li className="list__item">
+            <div className="list__button">
+              <img src="assets/dashboard.svg" className="list__img" />
+              <a href="/HomePage" className="nav__link">Inicio</a>
+            </div>
+          </li>
 
-            <li className="list__item">
-              <div className="list__button">
-                <FaPenClip />
-                <a href="/Crear" className="nav__link">Crear PQRS</a>
-              </div>
-            </li>
+          <li className="list__item">
+            <div className="list__button">
+              <FaPenClip />
+              <a href="/Crear" className="nav__link">Crear PQRS</a>
+            </div>
+          </li>
 
 
-            <li className="list__item">
-              <div className="list__button">
-                <FaMagnifyingGlass />
-                <a href="/Consultar" className="nav__link">Consultar PQRS</a>
-              </div>
-            </li>
+          <li className="list__item">
+            <div className="list__button">
+              <FaMagnifyingGlass />
+              <a href="/Consultar" className="nav__link">Consultar PQRS</a>
+            </div>
+          </li>
 
-            <li className="list__item">
-              <div className="list__button">
-                <ImExit className='icon' />
-                <a href="/Login" className="nav__link" onClick={handleLogout}>Salir</a>
-              </div>
-            </li>
+          <li className="list__item">
+            <div className="list__button">
+              <ImExit className='icon' />
+              <a href="/Login" className="nav__link" onClick={handleLogout}>Salir</a>
+            </div>
+          </li>
 
-          </ul>
-        </nav>
-      </div>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
@@ -117,91 +117,91 @@ export const MenuAdmin = () => {
     };
   }, []);
   return (
-      <div>
-        <nav className="nav">
-          <ul className="list">
+    <div>
+      <nav className="nav">
+        <ul className="list">
+          <Logo />
+          <li className="list__item">
+            <div className="list__button">
+              <img src="assets/dashboard.svg" className="list__img" />
+              <a href="/HomePagesAdmin" className="nav__link">Inicio</a>
+            </div>
+          </li>
+          <li className="list__item">
+            <div className="list__button">
+              <img src="assets/stats.svg" className="list__img" />
+              <a href="/Dashboard" className="nav__link">Estadisticas</a>
+            </div>
+          </li>
 
-            <li className="list__item">
-              <div className="list__button">
-                <img src="assets/dashboard.svg" className="list__img" />
-                <a href="/HomePagesAdmin" className="nav__link">Inicio</a>
-              </div>
-            </li>
-            <li className="list__item">
-              <div className="list__button">
-                <img src="assets/stats.svg" className="list__img" />
-                <a href="/Dashboard" className="nav__link">Estadisticas</a>
-              </div>
-            </li>
+          <li className="list__item list__item--click">
+            <div className="list__button list__button--click">
+              <RiContactsBook2Line />
+              <a href="#" className="nav__link">Gestion Usuario</a>
+              <img src="assets/arrow.svg" className="list__arrow" />
+            </div>
 
-            <li className="list__item list__item--click">
-              <div className="list__button list__button--click">
-                <RiContactsBook2Line />
-                <a href="#" className="nav__link">Gestion Usuario</a>
-                <img src="assets/arrow.svg" className="list__arrow" />
-              </div>
+            <ul className="list__show">
+              <li className="list__inside">
+                <a href="/VerUsuario" className="nav__link nav__link--inside">Ver Usuario</a>
+              </li>
 
-              <ul className="list__show">
-                <li className="list__inside">
-                  <a href="/VerUsuario" className="nav__link nav__link--inside">Ver Usuario</a>
-                </li>
+              <li className="list__inside">
+                <a href="/CrearUsuario" className="nav__link nav__link--inside">Crear Usuario</a>
+              </li>
+            </ul>
 
-                <li className="list__inside">
-                  <a href="/CrearUsuario" className="nav__link nav__link--inside">Crear Usuario</a>
-                </li>
-              </ul>
+          </li>
 
-            </li>
+          <li className="list__item list__item--click">
+            <div className="list__button list__button--click">
+              <RxDashboard />
+              <a href="#" className="nav__link">Gestion Categorias</a>
+              <img src="assets/arrow.svg" className="list__arrow" />
+            </div>
 
-            <li className="list__item list__item--click">
-              <div className="list__button list__button--click">
-                <RxDashboard />
-                <a href="#" className="nav__link">Gestion Categorias</a>
-                <img src="assets/arrow.svg" className="list__arrow" />
-              </div>
+            <ul className="list__show">
+              <li className="list__inside">
+                <a href="/VerCategoria" className="nav__link nav__link--inside">Ver Categorias</a>
+              </li>
 
-              <ul className="list__show">
-                <li className="list__inside">
-                  <a href="/VerCategoria" className="nav__link nav__link--inside">Ver Categorias</a>
-                </li>
+              <li className="list__inside">
+                <a href="/CrearCategoria" className="nav__link nav__link--inside">Crear Categorias</a>
+              </li>
+            </ul>
 
-                <li className="list__inside">
-                  <a href="/CrearCategoria" className="nav__link nav__link--inside">Crear Categorias</a>
-                </li>
-              </ul>
+          </li>
 
-            </li>
+          <li className="list__item list__item--click">
+            <div className="list__button list__button--click">
+              <IoHomeOutline />
+              <a href="#" className="nav__link">Gestion Dependencias</a>
+              <img src="assets/arrow.svg" className="list__arrow" />
+            </div>
 
-            <li className="list__item list__item--click">
-              <div className="list__button list__button--click">
-                <IoHomeOutline />
-                <a href="#" className="nav__link">Gestion Dependencias</a>
-                <img src="assets/arrow.svg" className="list__arrow" />
-              </div>
+            <ul className="list__show">
+              <li className="list__inside">
+                <a href="/VerDependencia" className="nav__link nav__link--inside">Ver Dependencias</a>
+              </li>
 
-              <ul className="list__show">
-                <li className="list__inside">
-                  <a href="/VerDependencia" className="nav__link nav__link--inside">Ver Dependencias</a>
-                </li>
+              <li className="list__inside">
+                <a href="/CrearDependencias" className="nav__link nav__link--inside">Crear Dependencias</a>
+              </li>
+            </ul>
 
-                <li className="list__inside">
-                  <a href="/CrearDependencias" className="nav__link nav__link--inside">Crear Dependencias</a>
-                </li>
-              </ul>
-
-            </li>
-            <li className="list__item">
-              <div className="list__button">
-                <ImExit className='icon' />
-                <a href="/Login" className="nav__link" onClick={handleLogout}>Salir</a>
-              </div>
-            </li>
+          </li>
+          <li className="list__item">
+            <div className="list__button">
+              <ImExit className='icon' />
+              <a href="/Login" className="nav__link" onClick={handleLogout}>Salir</a>
+            </div>
+          </li>
 
 
 
-          </ul>
-        </nav>
-      </div>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
@@ -239,34 +239,34 @@ export const MenuSecre = () => {
     };
   }, []);
   return (
-      <div>
-        <nav className="nav">
-          <ul className="list">
+    <div>
+      <nav className="nav">
+        <ul className="list">
+          <Logo />
+          <li className="list__item">
+            <div className="list__button">
+              <img src="assets/dashboard.svg" className="list__img" />
+              <a href="/HomePage" className="nav__link">Inicio</a>
+            </div>
+          </li>
 
-            <li className="list__item">
-              <div className="list__button">
-                <img src="assets/dashboard.svg" className="list__img" />
-                <a href="/HomePage" className="nav__link">Inicio</a>
-              </div>
-            </li>
+          <li className="list__item">
+            <div className="list__button">
+              <FaPenClip />
+              <a href="/GestionarPQRS" className="nav__link">Gestionar PQRS</a>
+            </div>
+          </li>
 
-            <li className="list__item">
-              <div className="list__button">
-                <FaPenClip />
-                <a href="/GestionarPQRS" className="nav__link">Gestionar PQRS</a>
-              </div>
-            </li>
+          <li className="list__item">
+            <div className="list__button">
+              <ImExit className='icon' />
+              <a href="/Login" className="nav__link" onClick={handleLogout}>Salir</a>
+            </div>
+          </li>
 
-            <li className="list__item">
-              <div className="list__button">
-                <ImExit className='icon' />
-                <a href="/Login" className="nav__link" onClick={handleLogout}>Salir</a>
-              </div>
-            </li>
-
-          </ul>
-        </nav>
-      </div>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
