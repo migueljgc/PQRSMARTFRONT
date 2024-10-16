bcafde7f81ca402c3630d9976699dad7cd2719c6
Jhojam1 <JhojamCaraballo@gmail.com>
2024-10-15 21:50:18 -0500

    Se Actualiza Dashboard Se Agregaron Graficos

--- src/Menus/Admin/Dashboard.jsx
+++ src/Menus/Admin/Dashboard.jsx
@@ -1,9 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import '../Admin/Dashboard.css'
+import '../Admin/Dashboard.css';
 import { MenuAdmin } from '../../componentes/Menu';
 import { UserinfoAmin } from '../../componentes/Userinfo';
 import axios from 'axios';
+import { Bar, Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    LineElement,
+    PointElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler,
+} from 'chart.js';
 
+// Registrar las escalas y componentes necesarios para los gráficos
+ChartJS.register(CategoryScale, LinearScale, BarElement, LineElement, Title, Tooltip, Legend, Filler, PointElement);
 
 const Dashboard = () => {
     const [pqrsData, setPqrsData] = useState([]);
@@ -11,32 +26,13 @@ const Dashboard = () => {
     const [totalPQRSPorDependencia, setTotalPQRSPorDependencia] = useState({});
     const [totalPQRSmes, setTotalPQRSmes] = useState({});
     const [totalPQRSDepMes, setTotalPQRSDepMes] = useState({});
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = '/Gradient.js'; // Ruta directa al archivo en public
-        script.async = true;
-        document.body.appendChild(script);
 
-        script.onload = () => {
-            // Inicializar el gradiente una vez que el script haya cargado
-            const gradient = new Gradient();
-            gradient.initGradient('#gradient-canvas');
-        };
-
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []); // Solo se ejecuta una vez al montar el componente
     useEffect(() => {
-        // Obtener datos de la API
         const fetchData = async () => {
             try {
                 const response = await axios.get('https://pqrsmartback-production.up.railway.app/api/request/get');
                 const data = response.data;
                 setPqrsData(data);
-                console.log(pqrsData)
-
-                // Calcular los totales
                 calcularTotales(data);
             } catch (error) {
                 console.error('Error fetching PQRS data:', error);
@@ -47,27 +43,25 @@ const Dashboard = () => {
     }, []);
 
     const calcularTotales = (data) => {
-        // Total de PQRS
         setTotalPQRS(data.length);
-        console.log(totalPQRSPorDependencia)
-        // Total de PQRS por dependencia
+
+        // Total PQRS por Dependencia
         const dependenciaCount = data.reduce((acc, pqrs) => {
             const dependencia = pqrs.dependence?.nameDependence || 'Sin Dependencia';
             acc[dependencia] = (acc[dependencia] || 0) + 1;
             return acc;
-        }, {});        
+        }, {});
         setTotalPQRSPorDependencia(dependenciaCount);
 
-        // Total de PQRS por mes
+        // Total PQRS por Mes
         const pqrsPorMes = data.reduce((acc, pqrs) => {
             const mes = new Date(pqrs.date).toLocaleString('default', { month: 'long', year: 'numeric' });
             acc[mes] = (acc[mes] || 0) + 1;
             return acc;
         }, {});
-        
         setTotalPQRSmes(pqrsPorMes);
 
-        // Total de PQRS por dependencia al mes
+        // Total PQRS por Dependencia al Mes
         const pqrsPorDepMes = data.reduce((acc, pqrs) => {
             const mes = new Date(pqrs.date).toLocaleString('default', { month: 'long', year: 'numeric' });
             const dependencia = pqrs.dependence?.nameDependence || 'Sin Dependencia';
@@ -75,9 +69,71 @@ const Dashboard = () => {
             acc[key] = (acc[key] || 0) + 1;
             return acc;
         }, {});
-        
         setTotalPQRSDepMes(pqrsPorDepMes);
     };
+
+    const dataChart = {
+        labels: Object.keys(totalPQRSmes),
+        datasets: [
+            {
+                label: 'Total PQRS por Mes',
+                data: Object.values(totalPQRSmes),
+                backgroundColor: 'rgba(75, 192, 192, 0.6)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const dataChartDependencia = {
+        labels: Object.keys(totalPQRSPorDependencia),
+        datasets: [
+            {
+                label: 'Total PQRS por Dependencia',
+                data: Object.values(totalPQRSPorDependencia),
+                backgroundColor: 'rgba(153, 102, 255, 0.6)',
+                borderColor: 'rgba(153, 102, 255, 1)',
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    // Datos para el gráfico de tendencia
+    const lineChartData = {
+        labels: Object.keys(totalPQRSmes),
+        datasets: [
+            {
+                label: 'Tendencia de PQRS',
+                data: Object.values(totalPQRSmes),
+                fill: true,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                tension: 0.1,
+            },
+        ],
+    };
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = '/Gradient.js'; // Ruta directa al archivo en public
+        script.async = true;
+
+        const handleLoad = () => {
+            // Inicializar el gradiente una vez que el script haya cargado
+            const gradient = new Gradient();
+            gradient.initGradient('#gradient-canvas');
+        };
+
+        script.onload = handleLoad;
+
+        document.body.appendChild(script);
+
+        return () => {
+            // Limpiar el script al desmontar
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
         <div className='Dashboard'>
             <canvas id="gradient-canvas" style={{ width: '100vw', height: '100vh', position: 'absolute', zIndex: -1 }}></canvas>
@@ -88,54 +144,49 @@ const Dashboard = () => {
                 <UserinfoAmin />
             </div>
             <div className="cuerpos">
-
                 <div className="dashform">
-
                     <h1 className="titledash">DASHBOARD</h1>
                     <div className="total">
                         <div className="formdash">
-                            <div className="totalprs">
-                                <h1 className="">Total PQRS</h1>
-                                {totalPQRS && <div className='pqrstotal'><p>{totalPQRS}</p> </div>}
+                            <div className="totalprs card">
+                                <h2><i className="fas fa-tasks"></i> Total PQRS</h2>
+                                {totalPQRS && <div className='pqrstotal'><p>{totalPQRS}</p></div>}
                             </div>
                             {Object.keys(totalPQRSPorDependencia).length > 0 && (
-                            <div className="totalpqrsdepe">
-                                <h1>Total PQRS por Dependencia:</h1>
-                                {Object.entries(totalPQRSPorDependencia).map(([dependencia, count]) => (
-                                    <div className='pqrstotalPorDependencia' key={dependencia}>
-                                        <p>{dependencia}: {count}</p>
-                                    </div>
-                                ))}
-                            </div>
-                        )}
+                                <div className="totalpqrsdepe card">
+                                    <h2><i className="fas fa-building"></i> Total PQRS por Dependencia:</h2>
+                                    <Bar data={dataChartDependencia} options={{ responsive: true }} />
+                                </div>
+                            )}
                             {Object.keys(totalPQRSmes).length > 0 && (
-                            <div className="totalpqrsmes">
-                                <h1>PQRS Por Mes:</h1>
-                                {Object.entries(totalPQRSmes).map(([mes, count]) => (
-                                    <div className='pqrstotalpqrsmes' key={mes}>
-                                        <p>{mes}: {count}</p>
-                                    </div>
-                                ))}
-                            </div>
-                        )}
-
-                        {Object.keys(totalPQRSDepMes).length > 0 && (
-                            <div className="totalpqrsdepemes">
-                                <h1>PQRS Por Dependencia al Mes:</h1>
-                                {Object.entries(totalPQRSDepMes).map(([key, count]) => (
-                                    <div className='pqrstotalpqrsdepemes' key={key}>
-                                        <p>{key}: {count}</p>
-                                    </div>
-                                ))}
-                            </div>
-                        )}
+                                <div className="totalpqrsmes card">
+                                    <h2><i className="fas fa-calendar-alt"></i> PQRS Por Mes:</h2>
+                                    <Bar data={dataChart} options={{ responsive: true }} />
+                                </div>
+                            )}
+                            {Object.keys(totalPQRSDepMes).length > 0 && (
+                                <div className="totalpqrsdepemes card">
+                                    <h2><i className="fas fa-chart-bar"></i> PQRS Por Dependencia al Mes:</h2>
+                                    {Object.entries(totalPQRSDepMes).map(([key, count]) => (
+                                        <div className='pqrstotalpqrsdepemes' key={key}>
+                                            <p>{key}: {count}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                            {/* Agregar el gráfico de tendencia aquí */}
+                            {Object.keys(totalPQRSmes).length > 0 && (
+                                <div className="tendenciaPQRS card">
+                                    <h2><i className="fas fa-trending-up"></i> Tendencia de PQRS:</h2>
+                                    <Line data={lineChartData} options={{ responsive: true }} />
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     );
-}
+};
 
 export default Dashboard;
